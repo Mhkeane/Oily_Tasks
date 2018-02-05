216af27fdf9383460a1172ece09caf3d26c34643
Mhkeane <mcraehk@gmail.com>
2018-02-05 20:46:27 +1300

    Add taskTwo initialisation

--- src/Two.kt
+++ src/Two.kt
@@ -5,9 +5,9 @@ import kotlin.test.assertEquals
 
 fun taskTwo(max: Int): Int{
     /**
-     * Returns the sum of the even numbers in the Fibonacci sequence that do not exceed max
+     * Returns the sum of the even numbers in the Fibonacci sequence (1, 2, 3, 5, 8, ...) that do not exceed max
      */
-    return 2
+    return 0
 }
 
 
@@ -19,7 +19,8 @@ class taskTwoTests(val X:Int, val Y:Int){
         @Parameterized.Parameters
         fun data() : List<Array<Int>> {
             return listOf(
-                    arrayOf(1, 2)
+                    arrayOf(1, 0),
+                    arrayOf(2, 2)
             )
         }
     }
