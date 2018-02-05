866201cee29953785258640e5a635678250aadb7
Mhkeane <mcraehk@gmail.com>
2018-02-05 22:37:20 +1300

    Add getPrimesUpToN function
    This function will be used to limit factor checking to prime numbers

--- src/Three.kt
+++ src/Three.kt
@@ -1,6 +1,7 @@
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import org.junit.Test as test
 
 fun taskThree(number: Long): Long{
@@ -21,6 +22,20 @@ fun taskThree(number: Long): Long{
         return number
     }
 
+}
+
+fun getPrimesUpToN(number: Long): MutableList<Long>{
+    var primes : MutableList<Long> = mutableListOf(1L,2L)
+    primes.addAll(3..number step 2)
+    var i: Int = 2
+    var nextPrime = 3L
+
+    while (i<number){
+        primes.removeAll { it % nextPrime == 0L }
+        nextPrime = primes[i]
+        i++
+    }
+    return primes
 }
 
 @RunWith(Parameterized::class)
@@ -46,5 +61,29 @@ class taskThreeTests(val X:Long, val Y:Long){
         val testOutput = Y
 
         assertEquals(testOutput, testInput)
+    }
+}
+
+@RunWith(Parameterized::class)
+class primeListTests(val X:Long, val Y:Long){
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() : List<Array<Long>> {
+            return listOf(
+                    arrayOf(1L, 1L),
+                    arrayOf(2L, 1L),
+                    arrayOf(2L, 2L)
+
+            )
+        }
+    }
+
+    @test fun primesLessThanXareY(){
+        val testInput = getPrimesUpToN(X)
+        val testOutput = Y
+
+        assertTrue(testOutput in testInput)
     }
 }
