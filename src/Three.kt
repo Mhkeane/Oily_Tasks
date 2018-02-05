3515b18e9268d82b5b288febbeb127ba94e19e2c
Mhkeane <mcraehk@gmail.com>
2018-02-05 23:09:34 +1300

    Fix getPrimesUpToN function

--- src/Three.kt
+++ src/Three.kt
@@ -30,8 +30,8 @@ fun getPrimesUpToN(number: Long): MutableList<Long>{
     var i: Int = 2
     var nextPrime = 3L
 
-    while (i<number){
-        primes.removeAll { it % nextPrime == 0L }
+    while (i<primes.size){
+        primes.removeAll { it % nextPrime == 0L && it > nextPrime}
         nextPrime = primes[i]
         i++
     }
@@ -73,8 +73,22 @@ class primeListTests(val X:Long, val Y:Long){
         fun data() : List<Array<Long>> {
             return listOf(
                     arrayOf(1L, 1L),
+
                     arrayOf(2L, 1L),
-                    arrayOf(2L, 2L)
+                    arrayOf(2L, 2L),
+
+                    arrayOf(3L, 1L),
+                    arrayOf(3L, 2L),
+                    arrayOf(3L, 3L),
+
+                    arrayOf(4L, 1L),
+                    arrayOf(4L, 2L),
+                    arrayOf(4L, 3L),
+
+                    arrayOf(5L, 1L),
+                    arrayOf(5L, 2L),
+                    arrayOf(5L, 3L),
+                    arrayOf(5L, 5L)
 
             )
         }
