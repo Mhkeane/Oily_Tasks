ccf4d0526afa6c15f05702b46e4e864bcc2b857d
Mhkeane <mcraehk@gmail.com>
2018-02-05 20:44:57 +1300

    Add taskTwo initialisation

---
+++ src/Two.kt
@@ -0,0 +1,33 @@
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.Test as test
+import kotlin.test.assertEquals
+
+fun taskTwo(max: Int): Int{
+    /**
+     * Returns the sum of the even numbers in the Fibonacci sequence that do not exceed max
+     */
+    return 2
+}
+
+
+@RunWith(Parameterized::class)
+class taskTwoTests(val X:Int, val Y:Int){
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() : List<Array<Int>> {
+            return listOf(
+                    arrayOf(1, 2)
+            )
+        }
+    }
+
+    @test fun sumOfEvenFibonacciUpToXisY(){
+        val testInput = taskTwo(X)
+        val testOutput = Y
+
+        assertEquals(testOutput, testInput)
+    }
+}
