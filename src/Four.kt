c5f05b14c10ff7dc4ac108948ac36ef10db4cbb6
Mhkeane <mcraehk@gmail.com>
2018-02-10 12:11:40 +1300

    First solve taskFour
    Brute force solution checking all possible combinations of integers

--- src/Four.kt
+++ src/Four.kt
@@ -7,8 +7,28 @@ fun taskFour(numDigits: Int): Int{
     /**
     Returns the largest palindrome that is made from the product of two integers of length numDigits
      */
-    return 9
+    val largestInteger = (Math.pow(10.0,numDigits.toDouble())).toInt() - 1
+    var possiblePalindromes: MutableList<Int> = mutableListOf()
+    for (i in (1..largestInteger)){
+        for (j in (1..largestInteger)){
+            if (isPalindrome(i*j)){
+                possiblePalindromes.add(i*j)
+            }
+        }
+    }
 
+    return possiblePalindromes.max()!!
+
+}
+
+fun isPalindrome(number: Int): Boolean{
+    val numberString = number.toString()
+    val numberLength:Int = numberString.length - 1
+    for (i in 0 until (numberLength)){
+        if (numberString[i] != numberString[numberLength-i])
+            return false
+    }
+    return true
 }
 
 @RunWith(Parameterized::class)
@@ -19,7 +39,9 @@ class taskFourTests(val X:Int, val Y:Int){
         @Parameterized.Parameters
         fun data() : List<Array<Int>> {
             return listOf(
-                    arrayOf(1, 9)
+                    arrayOf(1, 9),
+                    arrayOf(2, 9009),
+                    arrayOf(3, 906609)
 
             )
         }
@@ -27,6 +49,30 @@ class taskFourTests(val X:Int, val Y:Int){
 
     @test fun largestPalindromeThatIsProductOfIntsLengthXisY(){
         val testInput = taskFour(X)
+        val testOutput = Y
+
+        assertEquals(testOutput, testInput)
+    }
+}
+
+@RunWith(Parameterized::class)
+class isPalindromeTests(val X:Int, val Y:Boolean){
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() : List<Array<Any>> {
+            return listOf(
+                    arrayOf(1, true),
+                    arrayOf(21, false),
+                    arrayOf(11, true)
+
+            )
+        }
+    }
+
+    @test fun itIsYThatXIsAPalindrome(){
+        val testInput = isPalindrome(X)
         val testOutput = Y
 
         assertEquals(testOutput, testInput)
